8dcd2f4b0ae659cad79291aa7fe89b844bfdc990
Mateusz <mateuszzygadlo00@gmail.com>
2021-10-05 16:01:48 +0200

    Add Card component

---
+++ memory-card/src/components/App.js
@@ -0,0 +1,18 @@
+import Card from "./Card";
+
+const App = () => {
+    return(
+        <div className="container">
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+            <Card />
+        </div>
+    )
+}
+
+export default App;
